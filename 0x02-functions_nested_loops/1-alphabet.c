91510e3d03c7f130a8a91ca71cbaabf08768624d
Mbawin <gregorylamusah@gmail.com>
2023-08-15 11:04:57 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - print in lowercase
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char w;
+
+	for (w = 'a'; w <= 'z'; w++)
+	{
+	_putchar(w);
+	}
+	_putchar('\n');
+}
